6d375b91e3ad2b456d19bfb8db2c5010475162c7
Tayte Stokes <taytestokes9328@gmail.com>
2019-03-26 13:45:05 -0600

    added database with heroku and connected it using massive

--- server/index.js
+++ server/index.js
@@ -1,17 +1,33 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const socket = require('socket.io');
+const massive = require('massive');
 
 //Express Setup
 const app = express();
 app.use(bodyParser.json());
+
+//env file variables
+let {
+    DATABASE_CONNECTION
+} = process.env;
+
+//database connection using massive
+massive(DATABASE_CONNECTION).then(dbInstance => {
+    app.set('db', dbInstance);
+    console.log('database connected');
+}).catch(err => {
+    console.log(DATABASE_CONNECTION)
+    console.log('database connection failed', err);
+})
 
 
 const server = app.listen(4000, () => {
     console.log(`Server is running on port 4000`);
 });
 
-//Socket Setup
+//SOCKET SETUP
 const io = socket(server);
 
 //this function fires when the client connects to the server socket
